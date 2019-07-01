645e9ab0bd2eeeac25fbbb3da4d59199e7dce8bd
Haridoss23 <52314177+Haridoss23@users.noreply.github.com>
2019-07-01 10:52:07 +0530

    Update positivenegative.py

--- positivenegative.py
+++ positivenegative.py
@@ -1,8 +1,5 @@
-num = (int(input(" ")))
-if num > 0:
-   print("Positive")
-elif num == 0:
-   print("Zero")
-else:
-   print("Negative")
-   
+num=int(input("enter the number:"))
+ if(num/2)==0:
+      print("{0} is even.:format (num)")
+  else:
+      print("{0} is odd.:format (num)")
