ddc8c83c06a7dc8d4547a131377989e17307f9ea
Haridoss23 <52314177+Haridoss23@users.noreply.github.com>
2019-07-03 14:51:20 +0530

    Rename alphabet.py to lphabet1.py

---
+++ lphabet1.py
@@ -0,0 +1,9 @@
+print("Enter");
+b = input("character: ");
+if b == '0':
+    exit();
+else:
+    if((b>='a' and b<='z') or (b>='A' and b<='Z')):
+    	print(b, "is an alphabet.");
+    else:
+    	print(b, "is not an alphabet.");
